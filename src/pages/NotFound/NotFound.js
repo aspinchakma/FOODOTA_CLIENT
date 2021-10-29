5e770373a484a5cf4de5b7d97b3f5abe8616104b
aspinchakma <85893728+aspinchakma@users.noreply.github.com>
2021-10-30 04:32:58 +0600

    finished not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
         <div>
-            <h1>This is NotFound page bro</h1>
+            <div className="container row mx-auto main_error_container">
+                <div className="col-lg-6 text-center">
+                    <p className="error_status">404</p>
+                </div>
+                <div className="error_message_container col-lg-6">
+                    <h4>Foodota</h4>
+                    <p>Sorry sir, this page</p>
+                    <p>isn't available!</p>
+                    <Link path="/home"><button>VISIT HOME<i className="arrow_sign ms-3 fas fa-arrow-right"></i></button></Link>
+                </div>
+            </div>
         </div>
     );
 };
